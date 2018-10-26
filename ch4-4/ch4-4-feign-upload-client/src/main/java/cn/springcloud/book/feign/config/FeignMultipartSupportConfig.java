0bdba440a579fcdcac8a4f40630fd81d992afd35
wb-songwanke <13716891136@163.com>
2018-10-26 15:36:01 +0800

    feign文件上传添加日志

--- ch4-4/ch4-4-feign-upload-client/src/main/java/cn/springcloud/book/feign/config/FeignMultipartSupportConfig.java
+++ ch4-4/ch4-4-feign-upload-client/src/main/java/cn/springcloud/book/feign/config/FeignMultipartSupportConfig.java
@@ -14,6 +14,14 @@ import feign.codec.Encoder;
 @Configuration
 public class FeignMultipartSupportConfig {
 
+    /**
+     * 容器在接受到该类型的对象的请求的时候，会每次都重新生成一个新的对象给请求方，
+     * 虽然这种类型的对象的实例化以及属性设置等工作都是由容器负责的，
+     * 但是只要准备完毕，并且对象实例返回给请求方之后，容器就不在拥有当前对象的引用，
+     * 请求方需要自己负责当前对象后继生命周期的管理工作，包括该对象的销毁。
+     * 也就是说，容器每次返回请求方该对象的一个新的实例之后，就由这个对象“自生自灭”了。
+     * @return
+     */
     @Bean
     @Primary
     @Scope("prototype")
